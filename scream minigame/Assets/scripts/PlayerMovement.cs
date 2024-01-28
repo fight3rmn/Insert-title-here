4c1e1a181a0ea9d666cc7eb7fc03bc20b60eda9b
lboy22 <44554960+lboy22@users.noreply.github.com>
2024-01-27 16:09:48 -0800

    new variable

--- scream minigame/Assets/scripts/PlayerMovement.cs
+++ scream minigame/Assets/scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] int testing = 0;
+
     // Start is called before the first frame update
     void Start()
     {
